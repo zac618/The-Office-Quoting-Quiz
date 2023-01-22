036aaa77706440b91d5f1d968c3afb5ecf10de95
Zac E <zacegleston@gmail.com>
2023-01-22 13:33:08 -0600

    Troubleshooting API Issue

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-fetch('https://officeapi.dev/api/quotes/random')
-    .then(res => {
-        console.log(res);
-    })
+fetch("https://officeapi.dev/api/quotes/random", {
+    method: 'GET',
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+    },
+})
+.then(response => response.json())
+.then(response => console.log(JSON.stringify(response)))
+
