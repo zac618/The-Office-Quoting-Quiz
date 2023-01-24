a621392023e121d12cc18494c72347bce4b5f956
Zac E <zacegleston@gmail.com>
2023-01-23 21:34:13 -0600

    Added 2nd API for Incorrect Name

--- main.js
+++ main.js
@@ -10,36 +10,29 @@ function getQuote() {
         document.getElementById("quoteText").innerHTML="\""+quote+"\"";
         document.getElementById("nameOption1").innerHTML="-"+fullName;
         document.getElementById("nameOption2").innerHTML="-"+fullName;
+        console.log(fullName)
         // The above will likely need to be a separate api call to get a different random character as option 2
         // It might make sense to have 4 different options and make it a multiple choice?? Idk
-        console.log(quote)
-        console.log(fullName)
     })
 }
-
 getQuote()
 
-
-const myRequest = new Request("https://www.officeapi.dev/api/quotes/random");
-
-// fetch(myRequest, {
-//     method: 'GET',
-//     mode: 'no-cors',
-//     headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         "Content-Type": "application/json"
-//     },
-// })
-// .then((r) => {
-//     return r.json()
-// })
-// .then((data) => {
-//     console.log(data)
-// })
-
-// .then(data => console.log(data));
+function getName() {
+    let apiUrl = 'https://www.officeapi.dev/api/quotes/random';
+    fetch(apiUrl).then(response => {
+        return response.json();
+    }).then(data => {
+        let character=data['data']['character']['firstname'];
+        let lastName=data['data']['character']['lastname'];
+        let fullName=character+" "+lastName;
+        document.getElementById("nameOption2").innerHTML="-"+fullName;
+        console.log(fullName)
+        // The above will likely need to be a separate api call to get a different random character as option 2
+        // It might make sense to have 4 different options and make it a multiple choice?? Idk
+    })
+}
+getName()
 
 
-
-
-// .then(response => console.log(JSON.stringify(response)))
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!
+// HOW CAN I COMPARE THE 'FULL NAME' FROM getQuote TO THE 'FULL NAME' FROM getName
