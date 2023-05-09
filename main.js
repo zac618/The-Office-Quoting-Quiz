3846cbefdb67a19d9bde1fbeb1e32cce465c3fcd
Zac E <zacegleston@gmail.com>
2023-05-09 15:59:37 -0500

    Incorrect Options Pulling Through Names Now

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 import data from './quotes.json' assert { type: 'json' }
 
 
+// html elements
+const answerContainer = document.getElementById("answer-container");
 
 
-// allQuotes is storing all possible quote options
+// allQuotes is storing all possible quote options from the JSON file
 const allQuotes = []
-// allCharacters is storing all possible character options
+// allCharacters is storing all possible character options from the JSON file
 const allCharacters = []
-const characterPick = []
-
+// indexOfPick is an array containing the random index number generated
+const indexOfPick = []
+// randomIndex is just giving a random number between 0-49
+let randomIndex
+let wrongIndexRaw
+let wrongIndexUsed = []
+let wrongAnswers = []
 
 // confirmed working test variables
 const testQuote = data.dataJSON[9].quote
 const testChar = data.dataJSON[9].character
 
 
-
+// getAllCharacters is gathering all the quote values from the JSON file & pushing them to an array
 function getAllQuotes() {
     for (let i = 0 ; i < data.dataJSON.length ; i++) {
         let currentQuote = data.dataJSON[i].quote
@@ -23,33 +30,58 @@ function getAllQuotes() {
     }
 }
 
+// getAllCharacters is gathering all the character values from the JSON file & pushing them to an array
 function getAllCharacters() {
     for (let i = 0 ; i < data.dataJSON.length ; i++) {
         let currentcharacter = data.dataJSON[i].character
         allCharacters.push(currentcharacter)
     }
 }
+// getPickIndex is getting us a random number from 0-49 to use for getting a quote/character from JSON
+function getPickIndex() {
+    randomIndex = Math.floor(Math.random() * allCharacters.length)
+    indexOfPick.push(randomIndex);
+}
 
-function pickCharacter() {
-    const randomCharacterIndex = Math.floor(Math.random() * allCharacters.length)
-    const characterOption = characterPick.push(allCharacters[randomCharacterIndex]);
+function wrongAnswerIndex() {
+    for (let i = 0 ; i < 3 ; i++) {
+    wrongIndexRaw = Math.floor(Math.random() * allCharacters.length)
+    wrongIndexUsed.push(wrongIndexRaw)
+    }
 }
 
 
+function quoteForQuestion() {
+    return allQuotes[randomIndex]
+}
+
+function correctAnswerForQuestion() {
+    return allCharacters[randomIndex]
+}
+
+function wrongAnswersForQuestion() {
+    for (let i = 0 ; i < 3 ; i++) {
+        wrongAnswers.push(allCharacters[wrongIndexUsed[i]])
+    }
+}
 
 
+// function loadQuestion() {
+//     document.getElementById("quoteText").innerHTML=`${allQuotes[randomIndex]}`;
+//     document.getElementById("characterText1").innerHTML=`${allCharacters[randomIndex]}`;
 
+// }
 
 
 
 // NEED TO FIND A WAY TO DO THIS BUT MAKING THE HTML TAGS AUTOMATICALLY INCREMENT
 // ALSO NEED THE CHARACTERS TO AUTOMATICALLY POPULATE OBVIOUSLY...
 function testingQuote() {   
-    document.getElementById("quoteText").innerHTML=`${allQuotes[9]}`;
-    document.getElementById("characterText1").innerHTML=`${allCharacters[9]}`;
-    document.getElementById("characterText2").innerHTML=`${allCharacters[3]}`;
-    document.getElementById("characterText3").innerHTML=`${allCharacters[19]}`;
-    document.getElementById("characterText4").innerHTML=`${allCharacters[34]}`;
+    document.getElementById("quoteText").innerHTML=`${allQuotes[randomIndex]}`;
+    document.getElementById("characterText1").innerHTML=`${allCharacters[randomIndex]}`;
+    document.getElementById("characterText2").innerHTML=`${allCharacters[randomIndex]}`;
+    document.getElementById("characterText3").innerHTML=`${allCharacters[randomIndex]}`;
+    document.getElementById("characterText4").innerHTML=`${allCharacters[randomIndex]}`;
 
 }
 
@@ -67,10 +99,7 @@ function getCorrectAnswer() {
 // getCharacters function is also getting 3 random characters for incorrect options
 
 
-
-        document.getElementById("quoteText").innerHTML=`${testChar[0]}`;
-
-
+        // document.getElementById("quoteText").innerHTML=`${testChar[0]}`;
 
 
     //             possibleAnswers.push(randomCharacterPick)
@@ -88,14 +117,22 @@ function getCorrectAnswer() {
 
 
 function onInit(){ 
-    console.log(data.dataJSON.indexOf(testChar))
+
+    
+    // loadQuestion()
+
     getAllQuotes()
     getAllCharacters()
-    pickCharacter()
+    getPickIndex()
     testingQuote()
-    console.log(allQuotes[9])
-    console.log(allCharacters[9])
-    console.log(characterPick)
+    wrongAnswerIndex()
+    wrongAnswersForQuestion()
+    console.log(randomIndex)
+
+
+    console.log(quoteForQuestion())
+    console.log(correctAnswerForQuestion())
+    console.log(wrongAnswers)
 }
 
 onInit()
